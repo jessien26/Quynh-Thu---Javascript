5dbeb3524e552bc3be7c75da14a3a793a3b1e2b3
Jessie <jessienguyen2601@gmail.com>
2022-09-30 19:33:10 +0700

    Nguyen_Quynh_Thu_CO1

---
+++ C01/homework.js
@@ -0,0 +1,35 @@
+//Bài 1: Giới thiệu bản thân//
+console.log("Quynh Thu Nguyen")
+console.log("Nam sinh: 1998")
+console.log("Cung hoang dao: Bao Binh")
+
+//Bài 2: Phép chia ba số//
+let a = prompt("nhap a")
+let b = prompt("nhap b")
+let c = prompt("nhap c")
+console.log( "Ket qua la: " +
+    (a*a *b*b)/c
+)
+
+//Bài 3: Chu vi và diện tích hình tròn//
+
+let m = prompt("nhap ban kinh hinh tron:")
+const pi = 3.14
+console.log(
+    "dien tich hinh tron la: " + m*m*pi
+)
+console.log(
+    "chu vi hinh tron la: " + 2*a*pi
+)
+
+//Bài 4: Tính tiền//
+
+let x = prompt("nhap so luong hang: ")
+let y = prompt("nhap don gia: ")
+const t = 0.1
+
+console.log("Tong so tien cua ban la: "+ x*y+(x*y*t)
+)
+console.log("Thue gia tri gia tang: " + (x*y*t))
+
+//Bài 5: Số nút bảng số xe//
